df91c402ea27d8b2fd152f460df6e4d90c5c5a4a
Michael Hinson <michael.t.hinson@gmail.com>
2021-09-20 00:52:18 -0500

    Add comments to math methods

--- src/com/company/Math.java
+++ src/com/company/Math.java
@@ -3,9 +3,23 @@ package com.company;
 public class Math {
     public Math() {
     }
+
+    /**
+     * Takes two ints and returns the sum
+     * @param a int 1
+     * @param b int 2
+     * @return int 1 + int 2
+     */
     public int add(int a, int b){
         return a+b;
     }
+
+    /**
+     * Takes two ints and subtracts the second from the first
+     * @param a int 1
+     * @param b int 2
+     * @return int 1 - int 2
+     */
     public int subtract(int a, int b){
         return a-b;
     }
